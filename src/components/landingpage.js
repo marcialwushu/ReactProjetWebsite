3e2e701070c2839070bf75c0569236b3d1d15bf4
marcialwushu <marcialwushu@gmail.com>
2020-01-18 23:39:11 -0300

    LandingPage layout init

---
+++ src/components/landingpage.js
@@ -0,0 +1,24 @@
+import React,  { Component } from 'react';
+import {  Grid, Cell } from 'react-mdl';
+
+
+class Landing extends Component {
+	render() {
+		return(
+			<div>
+
+				<div style={{width: '100%', margin: 'auto'}}>
+				<Grid>
+					<Cell col={12}></Cell>
+
+				</Grid>
+
+
+			</div>
+
+			</div>
+		)
+	}
+}
+
+export default Landing;
---
+++ src/components/resume.js
@@ -0,0 +1,20 @@
+import React,  { Component } from 'react';
+import {  Grid, Cell } from 'react-mdl';
+
+
+class Resume extends Component {
+	render() {
+		return(
+			<div style={{width: '100%', margin: 'auto'}}>
+				<Grid>
+					<Cell col={12}></Cell>
+
+				</Grid>
+
+
+			</div>
+		)
+	}
+}
+
+export default Resume;
