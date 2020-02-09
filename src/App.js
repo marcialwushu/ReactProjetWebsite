7ecd3ce575c78c3410549e6ce0057cb2de925e7e
marcialwushu <marcialwushu@gmail.com>
2020-02-08 23:22:16 -0300

    layout navbarNavigation react-mdl add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import './App.css';
+import { Layout,Header, HeaderRow, Content, Textfield, Navigation, Drawer } from 'react-mdl';
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <div className="demo-big-content">
+    <Layout>
+        <Header title="Title" scroll>
+            <Navigation>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+            </Navigation>
+        </Header>
+        <Drawer title="Title">
+            <Navigation>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+            </Navigation>
+        </Drawer>
+        <Content>
+            <div className="page-content" />
+        </Content>
+    </Layout>
+</div>
   );
 }
 
